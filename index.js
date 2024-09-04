5e35e86f06604825553c4ce62e38608e78efbb21
Nadia <nh894@nyu.edu>
2024-09-04 16:00:14 -0400

    trending section added

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+const trendingEl = document.querySelector(".trending__list")
 const resultsEl = document.querySelector(".results__list");
+
+const searchBar = document.querySelector(".search__bar")
+searchBar.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        renderResults()
+    }
+})
 
 const options = {
     method: 'GET',
@@ -8,17 +16,45 @@ const options = {
     }
   };
 
+window.onload = async () => {
+    const trendingList = document.querySelector(".trending__list")
+    const trending = await fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
+    const trendingData = await trending.json();
+    const trendingDataArray = trendingData.results
+    trendingEl.innerHTML = trendingDataArray.map((result) => trendingHtml(result)).join("")
+  }
+
+function trendingHtml(result) {
+    return `<div class="trending__wrapper">
+        <div class="trending__img__wrapper">
+            <img src="https://image.tmdb.org/t/p/original/${result.poster_path}" alt="poster unavailable">
+        </div>
+            <div class="trending__title">
+                ${result.title}
+            </div>
+
+    </div>`
+    }
+
 async function renderResults() {
+    const trendingSection = document.getElementById("trending")
+    trendingSection.style.display = "none";
+    const resultsSection = document.getElementById("results")
+    resultsSection.style.display = "block";
+
     const resultsList = document.querySelector(".results__list")
     resultsList.classList.toggle("results__list__loading");
+
     const searchString = document.querySelector(".search__bar").value;
+    const heading = document.querySelector('.results__heading h3');
+    heading.innerHTML += `<span>"${searchString}"</span>`
+
     const results = await fetch(`https://api.themoviedb.org/3/search/movie?query=${searchString}&language=en-US&page=1`, options)
     const resultsData = await results.json();
     const resultsDataArray = resultsData.results
     if (results) {
         resultsList.classList.toggle("results__list__loading");
     }    
-
     resultsEl.innerHTML = resultsDataArray.map((result) => resultHtml(result)).join("");
   }
 
